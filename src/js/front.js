aad18cd90fef14ef309edfd73c2d0d75e6969b23
Mohamed Hasan <mhmdhasan7@outlook.com>
2020-02-22 12:12:58 +0200

    under construction page

--- src/js/front.js
+++ src/js/front.js
@@ -1,3 +1,12 @@
-$(function () {
 
+
+/* =========================================
+    COUNTDOWN 1
+========================================= */
+$('#getting-started').countdown('2020/3/1').on('update.countdown', function(event) {
+    var $this = $(this).html(event.strftime(''
+    + '<div class="holder"><span class="h1 number">%D</span> Day%!d</div>'
+    + '<div class="holder"><span class="h1 number">%H</span> Hr</div>'
+    + '<div class="holder"><span class="h1 number">%M</span> Min</div>'
+    + '<div class="holder"><span class="h1 number">%S</span> Sec</div>'));
 });
