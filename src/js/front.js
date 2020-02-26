2afe372a6bff3684b092093a41faeaa3a49810b8
Mohamed Hasan <mhmdhasan7@outlook.com>
2020-02-26 15:04:37 +0200

    bug fixes

--- src/js/front.js
+++ src/js/front.js
@@ -1,17 +1,3 @@
-
-
-/* =========================================
-    COUNTDOWN 1
-========================================= */
-// $('#getting-started').countdown('2020/3/1').on('update.countdown', function(event) {
-//     var $this = $(this).html(event.strftime(''
-//     + '<div class="holder"><span class="h1 number">%D</span> Day%!d</div>'
-//     + '<div class="holder"><span class="h1 number">%H</span> Hr</div>'
-//     + '<div class="holder"><span class="h1 number">%M</span> Min</div>'
-//     + '<div class="holder"><span class="h1 number">%S</span> Sec</div>'));
-// });
-
-
 $(function () {
     $('.hero-carousel').owlCarousel({
         animateOut: 'fadeOut',
@@ -28,11 +14,11 @@ $(function () {
             $('.d-none.d-lg-block').remove();
         }
     });
+
 });
 
 
-
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-var vh = window.innerHeight * 0.01;
+var vh = window.innerHeight * 0.03;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
