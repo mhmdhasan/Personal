554e03b465ff767f178a73196d600624ca8c665c
Mohamed Hasan <mhmdhasan7@outlook.com>
2020-03-05 21:51:23 +0200

    updates

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    $("#hireForm, #designForm").validate({
+    $("#hireForm, #designForm, #createForm, #bootstrapizeForm").validate({
         rules: {
             fullname: "required",
             email: {
