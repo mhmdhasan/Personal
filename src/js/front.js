d8cf6a3ba1af9fc09653685f0f43e2ff65c062f1
Mohamed Hasan <mhmdhasan7@oulook.com>
2021-06-27 21:13:51 +0200

    updates

--- src/js/front.js
+++ src/js/front.js
@@ -1,15 +1,17 @@
 $(function () {
 
+    let windowHeight = $(window).innerHeight();
+    $('.page-content').css('min-height', `${windowHeight}px`);
+
 
     $('a[href="#"]').on('click', function (e) {
         e.preventDefault();
     });
 
     $('.hero-carousel').owlCarousel({
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
         items: 1,
         autoplay: true,
+        smartSpeed: 700,
         loop: true,
         dots: false,
         mouseDrag: false,
@@ -22,9 +24,8 @@ $(function () {
     });
     $('.portfolio-carousel').owlCarousel({
         items: 1,
-        // autoplay: true,
         nav: false,
-        margin: 10,
+        margin: 0,
         navText: ['<i class="text-gradient fas fa-long-arrow-alt-left"></i>', '<i class="text-gradient fas fa-long-arrow-alt-right"></i>'],
         responsive: {
             991: {
@@ -52,11 +53,11 @@ $(function () {
     /* ===============================================================
         PAGE TRANSITION EFFECT
      =============================================================== */
-   $('.m-pagetransition').mPageTransition({
-       fadeOutTime: 600,
-       fadeInTime: 600
-   });
-
+   // $('.m-pagetransition').mPageTransition({
+   //     fadeOutTime: 600,
+   //     fadeInTime: 600
+   // });
+   //
 
    /* ===============================================================
          HUMBERGUR MENU ACTIVATE
@@ -112,25 +113,34 @@ $(function () {
     /* ===============================================================
     *   CUSTOM FILE INPUT
     * =============================================================== */
-    $(":file").filestyle({
-        buttonBefore: true,
-        btnClass: "btn-primary",
-        placeholder: "No file chosen"
-    });
+    let customeUploader = $(':file');
+    if (customeUploader) {
+        customeUploader.filestyle({
+            buttonBefore: true,
+            btnClass: "btn-primary",
+            placeholder: "No file chosen"
+        });
+    }
+
 
 });
 
 
+
+
+
 var textarea = document.querySelector('textarea');
-textarea.addEventListener('keydown', autosize);
-function autosize(){
-  var el = this;
-  setTimeout(function(){
-    el.style.cssText = 'height:auto; padding:0';
-    // for box-sizing other than "content-box" use:
-    // el.style.cssText = '-moz-box-sizing:content-box';
-    el.style.cssText = 'height:' + el.scrollHeight + 'px';
-  },0);
+if (textarea) {
+    textarea.addEventListener('keydown', autosize);
+    function autosize(){
+      var el = this;
+      setTimeout(function(){
+        el.style.cssText = 'height:auto; padding:0';
+        // for box-sizing other than "content-box" use:
+        // el.style.cssText = '-moz-box-sizing:content-box';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+      },0);
+    }
 }
 
 
@@ -138,3 +148,23 @@ function autosize(){
 var vh = window.innerHeight * 0.03;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+
+
+let moreSnippetsBtn = $('#moreSnippetsBtn');
+$.getJSON('js/snippets.json', function (data) {
+    let i = 0;
+    moreSnippetsBtn.on('click', function () {
+        snippet = `<div class="col-lg-4 mb-4"><div class="card card-animated border-0"><div class="card-body rounded shadow p-lg-5"><h3 class="h4">${data[i].title}</h3><p class="text-muted">${data[i].description}</p><a class="btn btn-primary btn-sm" href="${data[i].url}">View snippet</a></div></div></div>`;
+        snippet += `<div class="col-lg-4 mb-4"><div class="card card-animated border-0"><div class="card-body rounded shadow p-lg-5"><h3 class="h4">${data[i+1].title}</h3><p class="text-muted">${data[i+1].description}</p><a class="btn btn-primary btn-sm" href="${data[i+1].url}">View snippet</a></div></div></div>`;
+        snippet += `<div class="col-lg-4 mb-4"><div class="card card-animated border-0"><div class="card-body rounded shadow p-lg-5"><h3 class="h4">${data[i+2].title}</h3><p class="text-muted">${data[i+2].description}</p><a class="btn btn-primary btn-sm" href="${data[i+2].url}">View snippet</a></div></div></div>`;
+        $(snippet).insertBefore(moreSnippetsBtn.parent());
+        i = i + 3;
+
+        if (i > data.length - 1) {
+            moreSnippetsBtn.parent().hide();
+        }
+    });
+
+
+});
