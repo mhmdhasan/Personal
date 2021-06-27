cc40e7a3e92c434f02d4904109383fd56011e757
Mohamed Hasan <mhmdhasan7@oulook.com>
2021-06-27 22:28:27 +0200

    add particles.js

--- src/js/front.js
+++ src/js/front.js
@@ -144,13 +144,6 @@ if (textarea) {
 }
 
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-var vh = window.innerHeight * 0.03;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-
-
 let moreSnippetsBtn = $('#moreSnippetsBtn');
 $.getJSON('js/snippets.json', function (data) {
     let i = 0;
